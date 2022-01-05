d24cbda2ae2cce3984ac05bf82243ddd61b6cd29
hui.wang <hui.wang@funplus.com>
2022-01-05 12:53:26 +0800

    refactor: chain move into codec

--- secconf/codec.go
+++ secconf/codec.go
@@ -20,3 +20,21 @@ func (c CodecFuncChain) Apply(data []byte) (out []byte, err error) {
 type CodecFunc func(data []byte) ([]byte, error)
 
 func (f CodecFunc) Apply(data []byte) ([]byte, error) { return f(data) }
+
+// gip => encrypt => base64
+func StandardChainEncode(encrypt CodecFunc) Codec {
+	return CodecFuncChain([]CodecFunc{
+		EncoderGZip,
+		encrypt,
+		EncoderBase64,
+	})
+}
+
+// base64 => decrypt => gzip
+func StandardChainDecode(decrypt CodecFunc) Codec {
+	return CodecFuncChain([]CodecFunc{
+		DecoderBase64,
+		decrypt,
+		DecoderGZip,
+	})
+}
